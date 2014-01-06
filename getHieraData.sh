7a26f6f5919183cd9feeef96998341418ce47d05
pderoxas <paolo.deroxas@gmail.com>
2014-01-06 16:05:53 -0500

    refactor logic into function

--- getHieraData.sh
+++ getHieraData.sh
@@ -3,53 +3,54 @@
 #http://stedolan.github.io/jq/
 
 #the base url of the git repositories
-BASE_URL="http://192.168.1.13:8080/paypal-hiera/hieraData"
-CUSTOM_HIERA_DIR="/etc/puppet/hieradata/env/DEV/store_configs"
+BASE_URL="http://192.168.1.5:8080/paypal-hiera/hieraData"
+STORE_HIERA_DIR="/etc/puppet/hieradata/env/DEV/store_configs"
 GROUP_HIERA_DIR="/etc/puppet/hieradata/env/DEV/group_configs"
-DEFAULT_HIERA_DIR="/etc/puppet/hieradata/env/DEV/geo_location_configs"
+LOCATION_HIERA_DIR="/etc/puppet/hieradata/env/DEV/geo_location_configs"
 
+function saveHieraFiles() {
+  curHieraDir=$1
+  curData=$2
+  #echo $curHieraDir
+  #echo $curData
+
+  #purge existing files
+  find $curHieraDir -mindepth 1 -delete
+  
+  #save each config as .json
+  numberOfConfigs=$(echo $curData | jq '. | length')
+  for ((i = 0; i <= numberOfConfigs - 1; i++))
+  do
+    curConfig=$(echo $curData | jq ".[${i}]")
+    curId=$(echo $curConfig | jq ".id" | sed -e 's/^"//'  -e 's/"$//')
+    echo $curConfig>$curHieraDir/$curId.json
+  done
+
+}
 
 #construct the releases url
 hiera_url="$BASE_URL"
 echo "Hiera URL: $hiera_url"
 hiera_data=$(curl $hiera_url)
-custom_data=$(echo $hiera_data | jq ". | .customConfigs")
+store_data=$(echo $hiera_data | jq ". | .storeConfigs")
 group_data=$(echo $hiera_data | jq ". | .groupConfigs")
-default_data=$(echo $hiera_data | jq ". | .defaultConfigs")
+location_data=$(echo $hiera_data | jq ". | .locationConfigs")
 
 
 #echo
-#echo "CUSTOM=$custom_data"
+#echo "STORE=$store_data"
 #echo 
 #echo "GROUP=$group_data"
 #echo
-#echo "DEFAULT=$default_data"
+#echo "LOCATION=$location_data"
 #echo
 
-find $CUSTOM_HIERA_DIR -mindepth 1 -delete
-numberOfCustom=$(echo $custom_data | jq '. | length')
-for ((i = 0; i <= numberOfCustom - 1; i++))
-do
-  curConfig=$(echo $custom_data | jq ".[${i}]")
-  curId=$(echo $curConfig | jq ".id" | sed -e 's/^"//'  -e 's/"$//')
-  echo $curConfig>$CUSTOM_HIERA_DIR/$curId.json
-done
+#save store hiera data
+saveHieraFiles "$STORE_HIERA_DIR" "$store_data"
 
-find $GROUP_HIERA_DIR -mindepth 1 -delete
-numberOfGroup=$(echo $group_data | jq '. | length')
-for ((i = 0; i <= numberOfGroup - 1; i++))
-do
-  curConfig=$(echo $group_data | jq ".[${i}]")
-  curId=$(echo $curConfig | jq ".id" | sed -e 's/^"//'  -e 's/"$//')
-  echo $curConfig>$GROUP_HIERA_DIR/$curId.json
-done
+#save group hiera data
+saveHieraFiles "$GROUP_HIERA_DIR" "$group_data"
 
-find $DEFAULT_HIERA_DIR -mindepth 1 -delete
-numberOfDefault=$(echo $default_data | jq '. | length')
-for ((i = 0; i <= numberOfDefault - 1; i++))
-do
-  curConfig=$(echo $default_data | jq ".[${i}]")
-  curId=$(echo $curConfig | jq ".id" | sed -e 's/^"//'  -e 's/"$//')
-  echo $curConfig>$DEFAULT_HIERA_DIR/$curId.json
-done
+#save location hiera data
+saveHieraFiles "$LOCATION_HIERA_DIR" "$location_data"
 
